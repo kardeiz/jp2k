f6fd7f95a4e762e84c95aabe219c7a36f93294e0
Jacob Brown <kardeiz@gmail.com>
2019-10-03 14:15:21 -0500

    cleanup example

--- examples/decode.rs
+++ examples/decode.rs
@@ -1,21 +1,5 @@
-// fn main() {
-//     let bytes = include_bytes!("./rust-logo-512x512-blk.jp2");
-
-//     let jp2k::Image(img) = jp2k::Image::from_bytes(
-//         bytes,
-//         jp2k::Codec::JP2,
-//         Some(jp2k::DecodeParams::default().with_decoding_area(0, 0, 256, 256))
-//     )
-//     .unwrap();
-
-//     let mut output = std::path::Path::new("examples/output/result.png");
-//     let _ = img.save(&mut output);
-// }
-
 fn main() {
-    // let bytes = include_bytes!("/mnt/c/projects/jp2k/examples/rust-logo-512x512-blk.jp2");
 
-    // let bytes: std::sync::Arc<[u8]> = bytes.to_vec().into();
 
     let bytes = include_bytes!("/mnt/c/projects/iiif-server/cache/remote/45928.jp2");
 
@@ -23,21 +7,9 @@ fn main() {
     let codec = jp2k::Codec::create(jp2k::CODEC_FORMAT::OPJ_CODEC_JP2).unwrap();
     let stream = jp2k::Stream::from_bytes(bytes).unwrap();
 
-    // let y = jp2k::DecodeParams::default();
-    // // let stream = jp2k::Stream::from_file("/mnt/c/projects/jp2k/examples/rust-logo-512x512-blk.jp2").unwrap();
-
-    // // let img = jp2k::Image::info_only(codec, stream).unwrap();
-
-
-    // let x  =jp2k::Image::info_on/**/ly(codec, stream).unwrap();
-
-
-    // println!("{:?}", &y);
-
 
     let jp2k::ImageBuffer { buffer, width, height, num_bands } = jp2k::ImageBuffer::build(
         jp2k::DecodeParams::default().with_reduce_factor(3), codec, stream).unwrap();
-    // println!("{:?}", bytes.len());
 
     let img = rips::Image::from_memory(
         buffer,
@@ -48,8 +20,6 @@ fn main() {
     ).unwrap();
 
     img.write_to_file("test.png").unwrap();
-
-    // println!("{:?}", unsafe { (&*img.0) });
 
 
 }
